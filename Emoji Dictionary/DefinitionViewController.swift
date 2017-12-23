91615143d2d60c2478711857ce338605592e4471
Joey Newfield <joey.newfield@gmail.com>
2017-12-23 15:17:12 -0600

    More descriptions/definitions

--- Emoji Dictionary/DefinitionViewController.swift
+++ Emoji Dictionary/DefinitionViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class DefinitionViewController: UIViewController {
 
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var adjectiveLabel: UILabel!
     @IBOutlet weak var definitionLabel: UILabel!
     @IBOutlet weak var emojiLabel: UILabel!
     var emoji = "NO EMOJI"
@@ -22,26 +24,38 @@ class DefinitionViewController: UIViewController {
         
         if emoji == "😇" {
             definitionLabel.text = "Sarah"
+            adjectiveLabel.text = "Known to others as 'sweet'"
+            ageLabel.text = "Age: 14"
         }
         
         if emoji == "😱" {
             definitionLabel.text = "Daddy"
+            adjectiveLabel.text = "Known to others as 'broken'"
+            ageLabel.text = "Age: 185"
         }
         
         if emoji == "🤥" {
             definitionLabel.text = "Joey"
+            adjectiveLabel.text = "Known to others as 'dumb, really really dumb'"
+            ageLabel.text = "Age: 24"
         }
         
         if emoji == "🤩" {
             definitionLabel.text = "Mommy"
+            adjectiveLabel.text = "Known to others as '#1 Mommy and baker'"
+            ageLabel.text = "Age: 25,894"
         }
         
         if emoji == "🤬" {
             definitionLabel.text = "Austin"
+            adjectiveLabel.text = "Known to others as 'who?'"
+            ageLabel.text = "Age: 25"
         }
         
         if emoji == "🐶" {
             definitionLabel.text = "Petey"
+            adjectiveLabel.text = "Known to others as 'asleep'"
+            ageLabel.text = "Age: Unknown"
         }
        
     }
